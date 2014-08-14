bab91156b01aefa4f0962793aeb172b3d88f2db4
Kenshinn <kenshinnn@gmail.com>
2014-08-15 04:04:15 +0800

    fix utf8 cue problem

--- extract.sh
+++ extract.sh
@@ -12,8 +12,13 @@ zipmv gbk
 		cd $j
 		cue=`ls -1 *.cue|head -n 1`
 		cueu=utf8$cue
+		isutf8 $cue
+		if [ "$?" = "0" ]; then
+			awk '{if(NR==1)sub(/^\xef\xbb\xbf/,"");print}' $cue > $cueu
+		else
+			cat $cue| iconv -f gbk -t utf8 -c > $cueu
+		fi
 		img=`ls -1 *.ape|head -n 1`
-		cat $cue | iconv -f gbk -t utf8 -c > $cueu
 		musicsplit ape $cueu $img
 		cuetag $cueu *.flac
 		cd ..
